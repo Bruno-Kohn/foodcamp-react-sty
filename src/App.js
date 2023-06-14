7c7ad373fad6f132a5fa5f2b163b3ae08c2d03fd
Bruno-Kohn <brunokohn1993@gmail.com>
2023-06-13 22:41:42 -0300

    feat: create divs for items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Top from './Top/Top';
+import Content from './Content/Content';
 import Bottom from './Bottom/Bottom';
 
 function App() {
   return (
     <>
       <Top />
+      <Content />
       <Bottom />
     </>
   );
---
+++ src/Content/Content.js
@@ -0,0 +1,30 @@
+export default function Content() {
+  return (
+    <div className='content'>
+      <div className='item-option'>
+        <div className='item-option-description'>
+          <h1>Primeiro, monte seu prato</h1>
+        </div>
+        <div className='items-card'>
+          <div className='card'></div>
+        </div>
+      </div>
+      <div className='item-option'>
+        <div className='item-option-description'>
+          <h1>Primeiro, monte seu prato</h1>
+        </div>
+        <div className='items-card'>
+          <div className='card'></div>
+        </div>
+      </div>
+      <div className='item-option'>
+        <div className='item-option-description'>
+          <h1>Primeiro, monte seu prato</h1>
+        </div>
+        <div className='items-card'>
+          <div className='card'></div>
+        </div>
+      </div>
+    </div>
+  );
+}
